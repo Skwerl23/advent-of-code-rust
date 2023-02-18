ad5759faa69954d7e535c0aa79de1bf85370b587
Skwerl23 <cottermannelson@gmail.com>
2023-02-18 14:22:52 -0700

    solved days 9 and 10
    Solved days 9 and 10.
    Day 9 I took a new approach to run the test data first, I like it.

--- src/year15/day09.rs
+++ src/year15/day09.rs
@@ -1,11 +1,74 @@
-// use std::fs::read_to_string;
+use std::fs::read_to_string;
+use std::collections::HashSet;
 
 pub fn day09() {
     //import data file
-    // let input = read_to_string(r#"c:\tools\adventofcode\2015\input02.txt"#).expect("Failed to read file");
+    let input = read_to_string(r#"c:\tools\adventofcode\2015\input09.txt"#).expect("Failed to read file");
+    let test_input_1 = "London to Dublin = 464\nLondon to Belfast = 518\nDublin to Belfast = 141".to_string();
+    let answer:(i32, i32);
+    let test_answer: (i32, i32) = solve_day09(test_input_1);
+    if test_answer == (605, 982) {
+        println!("Test case solved correctly. The solutions are...");
+        answer = solve_day09(input);
+        println!("Answer1 = {}", answer.0);
+        println!("Answer2 = {}", answer.1);
+    }
+    else {
+        println!("Test case failed to solve. Try again.");
+        println!("Test Answer1 = {}", test_answer.0);
+        println!("Test Answer2 = {}", test_answer.1);
 
-    //print answers.
-//    println!("Answer 1 = {answer1}");
-//    println!("Answer 2 = {answer2}");
+    }
 
+}
+
+fn solve_day09(input: String) -> (i32, i32) {
+    let input_lines = input.split_terminator('\n');
+    let mut distance_vectors:Vec<(&str, &str, i32)> = Vec::new();
+
+    let mut destinations: HashSet<&str> = HashSet::new();
+    for line in input_lines {
+        let data_vec: Vec<&str> = line.split(' ').collect();
+        destinations.insert(data_vec[2]);
+        destinations.insert(data_vec[0]);
+        let left = data_vec[0];
+        let right = data_vec[2];
+        let dist: i32 = data_vec[4].parse::<i32>().unwrap();    
+        distance_vectors.push((left, right, dist));
+        distance_vectors.push((right, left, dist));
+    }
+    let mut seen: HashSet<&str> = HashSet::new();
+    let mut min_travel_distance: i32 = i32::max_value();
+    let mut max_travel_distance: i32 = 0;
+    for destination in destinations.iter() {
+        seen.insert(&destination);
+        find_shortest_path(destination, &mut seen, 0, &mut min_travel_distance, &mut max_travel_distance, &destinations, &distance_vectors);
+        seen.remove(destination);
+    }
+    // answer = TSP();
+    (min_travel_distance, max_travel_distance)
+}
+
+fn find_shortest_path<'a> (current_location: &str, mut seen: &mut HashSet<&'a str>, current_travel_distance: i32, min_travel_distance: &mut i32, max_travel_distance: &mut i32, destinations: &HashSet<&str>, distance_vectors: &Vec<(&str, &'a str, i32)>)  {
+    if &seen.len() == &destinations.len() {
+        // println!("{:?},{}", seen, current_travel_distance);
+        if current_travel_distance < *min_travel_distance {
+            *min_travel_distance = current_travel_distance;
+        }
+        if current_travel_distance > *max_travel_distance {
+            *max_travel_distance = current_travel_distance;
+        }
+        
+    }
+    for route in distance_vectors {
+        let (left, right, distance) = &route;
+        
+        if left == &current_location {
+            if !seen.contains(right) {
+                seen.insert(right.clone());
+                find_shortest_path(right, &mut seen, current_travel_distance+distance, min_travel_distance, max_travel_distance, &destinations, &distance_vectors);
+                seen.remove(right.clone());
+            }
+        }
+    }
 }
--- src/year15/day10.rs
+++ src/year15/day10.rs
@@ -1,11 +1,42 @@
-// use std::fs::read_to_string;
 
 pub fn day10() {
-    //import data file
-    // let input = read_to_string(r#"c:\tools\adventofcode\2015\input02.txt"#).expect("Failed to read file");
+    let mut input = "3113322113".to_string();
+    // the way our loop works, we add a zero at the end so we don't cause a "index out of bounds" panic
+    const ITERATIONS:i32 = 51; //they said 40 iterations, but it needs 41? 0 to 40 run which is 41 iterations. / 51 for part 2. 
+    let mut it_count = 0;
+    //curiously this gets set once, but it wants it to be mutable
+    let mut answer1: usize = 0;
+    let mut answer2: usize = 0;
+    while it_count < ITERATIONS {
+        let numbers: Vec<char> = input.chars().collect();
+        let mut count = 1;
+        input = "".to_string();
+        for i in 0..numbers.len() {
+            //if we're on the last digit, we can't compare to nothing after. or we get an index out of bounds
+            //the first if check fails, so it doesn't try the second one. and runs the else statement.
+            if i != numbers.len()-1 && numbers[i] == numbers[i+1]{
+                count+=1;
+            }
+            else {
+                input+=format!("{}{}",count, numbers[i]).as_str();
+                count = 1;
+            }
 
-    //print answers.
-//    println!("Answer 1 = {answer1}");
-//    println!("Answer 2 = {answer2}");
+        }
+        // add a zero to prevent out of bounds panic.
+        if it_count == 40 {
+            answer1 = numbers.len();
+        }
+        if it_count == 50 {
+            answer2 = numbers.len();
+            break;
+        }
+
+        it_count += 1;
+    }
+    // 252594 is too small
+    // 329356 is the right answer? 
+   println!("Answer 1 = {answer1}");
+   println!("Answer 2 = {answer2}");
 
 }
