3d9cf17c4356fbc0da6cf452bc5778340940d326
Skwerl23 <cottermannelson@gmail.com>
2023-02-20 00:10:56 -0700

    Solved day 14
    Solved day 14. Interesting problem. the part 2 threw a reverse uno on my logic, so I had to re-write my code.

--- src/year15/day14.rs
+++ src/year15/day14.rs
@@ -1,15 +1,20 @@
+use std::fs::read_to_string;
+use std::collections::HashMap;
+
 pub fn day14() {
     println!("This problem hasn't been solved. this is a copy of day 11 for the time being");
-    let input = "hxbxwxba".to_string();
-    let test_input = "ghijklmn".to_string();
+    let input  = read_to_string(r#"c:\tools\adventofcode\2015\input14.txt"#).expect("Failed to read file.");
+    let test_input = "Comet can fly 14 km/s for 10 seconds, but then must rest for 127 seconds.\nDancer can fly 16 km/s for 11 seconds, but then must rest for 162 seconds.".to_string();
     //curiously this gets set once, but it wants it to be mutable
-    let test_answer = solve_day14(test_input);
+    let test_answer = solve_day14(test_input, 1000);
     //get both test_answers, for validation
-    if test_answer == ("ghjaabcc".to_string(), "ghjbbcdd".to_string()) {
+    if test_answer == (1120, 689) {
         //explain passing, and run final test
         println!("Test case passed. Running actual input...");
-        let answer = solve_day14(input);
+        let answer = solve_day14(input, 2503);
         println!("Answer1 = {}", answer.0);
+        //648 too low: why?
+        //647 too low...
         println!("Answer2 = {}", answer.1);
     } 
     else {
@@ -21,86 +26,75 @@ pub fn day14() {
     }
 }
 
-fn solve_day14(input: String) -> (String, String) {
-    //this function increments string letter by letter.
-    let mut chars: Vec<char> = input.chars().collect();
-    // let mut count = 0;
-    let char_len = &chars.len();
-    let mut run = 0;
-    let mut answer1: Vec<char> = Vec::new();
-    let mut answer2: Vec<char> = Vec::new();
-    while run < 2 {
-        if valid_chars(&chars) {
-            if run == 0 {
-                answer1 = chars.clone();
-            } else {
-                answer2 = chars.clone();
+fn solve_day14(input: String, timelimit: i32) -> (i32, i32) {
+    let split_input: Vec<&str> = input.split_terminator('\n').collect();
+    //each half is completely different and works differently, thus 2 different functions
+
+    //this function loops for each second, and determines the reindeers distance, and the winner
+    //need a racing hashmap and point hashmap... 
+    let mut race_hashmap:HashMap<String, (i32, i32, bool)> = HashMap::new();
+    let mut point_hashmap:HashMap<String, i32> = HashMap::new();
+    let mut racers_stats: Vec<(String, i32, i32, i32)> = Vec::new();
+    //by moving every parsing step into this loop, we reduced processing time from 13ms to 2ms - particularly racers_stats
+    for line in &split_input {
+        let split_line: Vec<&str> = line.split_whitespace().collect();
+        let name: String = split_line[0].to_string();
+        let speed: i32 = split_line[3].parse().unwrap();
+        let run_time: i32 = split_line[6].parse().unwrap();
+        let rest_time: i32 = split_line[13].parse().unwrap();
+        race_hashmap.insert(name.clone(), (0, 0, true));
+        point_hashmap.insert(name.clone(), 0);
+        racers_stats.push((name, speed, run_time, rest_time));
+    }
+    for _ in 0..timelimit {
+        let mut winner: i32 = 0;
+        // let mut winner_name: String = String::new();
+        for line in &racers_stats {
+            //we could probably create an array of tuples of this info, but yea.
+            let (name, speed, run_time, rest_time) = line;
+            let (current_distance, current_time, moving) = race_hashmap.get_mut(name).unwrap();
+            if *moving {
+                *current_distance += *speed;
+                *current_time += 1;
+                if *current_time == *run_time {
+                    *current_time = 0;
+                    *moving = false;
+                }
             }
-            run += 1;
-        }
-        // count+=1;
-        chars[char_len-1] = (chars[char_len-1] as u8 + 1) as char;
-        for i in 1..chars.len() {
-            // println!("current char is {}",chars[i]);
-            if chars[i] == '{' {
-                // println!("This happened");
-                chars[i] = 'a';
-                chars[i-1] = (chars[i-1] as u8 + 1) as char;
+            else {
+                *current_time += 1;
+                if *current_time == *rest_time {
+                    *current_time = 0;
+                    *moving = true;
+                }
+
+            }
+            // race_hashmap[&name] = (current_distance, current_time, moving);
+            if *current_distance > winner {
+                winner = *current_distance;
+            }
+        } 
+        //dereference and modify winner with an additional point.
+        //could have used the original hashmap, but i decided to split their scores for simplicity.
+        for (k, v) in &race_hashmap {
+
+            let (distance,_,_) = *v;
+            if distance == winner {
+                *point_hashmap.get_mut(k).unwrap() += 1;
             }
 
         }
-        // println!("{:?}", chars);
+
     }
-    (answer1.into_iter().collect(), answer2.into_iter().collect())
-
-
-}
-
-
-fn valid_chars(chars: &Vec<char>)-> bool {
-    //this function checks if the string of characters is a vlid string.
-    //first check is if it has a incrementing 3 characters in a row.
-    let mut three_increment = false;
-
-    for i in 0..chars.len()-3 {
-        if chars[i] as u8 + 1 == chars[i+1] as u8  && chars[i] as u8 + 2 == chars[i+2] as u8 {
-            three_increment = true;
-            break
-        }
+    //initialize answers
+    let mut answer1 = 0;
+    let mut answer2 = 0;
+    for (k, v) in point_hashmap {
+        //split race_hashmap from key and get winner distance for part 1
+        let (distance,_,_) = race_hashmap[&k];
+        answer1 = i32::max(distance, answer1);
+        //check point value for part 2 and find winner distance
+        answer2 = i32::max(v, answer2);
     }
-
-    //second check validates if there are 2 pairs.
-    //basically if the next character is equal to this character we have one pair, go ahead and skip the next loop
-    //if we find at least one more pair our count is 2+
-    //2+ = good
-    let mut skip = false;
-    let mut double_match_count = 0;
-    for i in 0..chars.len()-1 {
-        if skip {
-            skip = false;
-            continue;
-        }
-        if chars[i] == chars[i+1] {
-            double_match_count+=1;
-            skip = true;
-        }
-    }
-
-    //final rule is that it can't contain i o or l due to confuse. (I O or L)
-    let mut bad_chars: bool = true;
-    for i in ['i','o','l'] {
-        if chars.contains(&i) {
-            bad_chars = false;
-        }
-    }
-
-    //if all 3 are true, set answer to true. and finally return answer.
-    let answer: bool;
-    if three_increment && bad_chars && double_match_count > 1{
-        answer = true;
-    }
-    else {
-        answer = false;
-    }
-    answer
+    (answer1, answer2)   
 }
